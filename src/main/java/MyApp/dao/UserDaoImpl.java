652def4c8ffeef3fd6fe3a74a780c41968dd3471
Maxim Ostanin <ostanin.maks@gmail.com>
2023-07-12 11:38:28 +0300

    fifth commit

--- src/main/java/MyApp/dao/UserDaoImpl.java
+++ src/main/java/MyApp/dao/UserDaoImpl.java
@@ -5,18 +5,15 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
-@Transactional
 public class UserDaoImpl implements UserDao {
 
     @PersistenceContext
     private EntityManager em;
 
     @Override
-    @Transactional
     public void addUser(String name, String lastname, int age) {
 
         User user = new User(name, lastname, age);
@@ -25,7 +22,6 @@ public class UserDaoImpl implements UserDao {
     }
 
     @Override
-    @Transactional
     public void removeUserById(int id) {
 
         User user = em.find(User.class, id);
@@ -34,21 +30,18 @@ public class UserDaoImpl implements UserDao {
     }
 
     @Override
-    @Transactional
     public List<User> getAllUsers() {
 
         return em.createQuery("from User", User.class).getResultList();
     }
 
     @Override
-    @Transactional
     public User getUserById(int id) {
 
         return em.find(User.class, id);
     }
 
     @Override
-    @Transactional
     public void updateUser(int id, String name, String lastname, int age) {
 
         User user = em.find(User.class, id);
